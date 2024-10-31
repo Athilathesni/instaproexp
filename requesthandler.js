85854f4b91590b3d59b35fa47da8f41889557987
Athilathesni <usmanchusman606@gmail.com>
2024-10-31 14:10:39 +0530

    file added

---
+++ requesthandler.js
@@ -0,0 +1,108 @@
+import postSchema from './model/post.js'
+import userSchema from './model/user.js'
+import bcrypt from 'bcrypt'
+
+import pkg from 'jsonwebtoken'
+const {sign} =pkg
+
+
+export async function login(req,res) {
+    const {email,pass}=req.body
+    if(!(email&&pass))
+        return res.status(500).send({msg:"empty input"})
+
+    const user= await userSchema.findOne({email})
+    if(!user)
+        return res.status(500).send({msg:"not exist"})
+
+    const success=await bcrypt.compare(pass,user.pass)
+
+    if(success!=true)
+        return res.status(500).send({msg:"Incorrect Password"})
+
+    const token=await sign({UserID:user._id},process.env.jwt_key,{expiresIn:"24h"})
+    res.status(200).send({token})
+}
+
+export async function getUser(req, res) {
+    const usr=await userSchema.findOne({_id:req.user.UserID})
+    const data=await postSchema.find()
+    res.status(200).send({usr,data}); 
+}
+
+export async function getUserDetails(req,res) {
+    const usr=await userSchema.findOne({_id:req.user.UserID})
+    const post=await postSchema.find({id:req.user.UserID})
+    res.status(200).send({usr,post}); 
+}
+
+
+
+export async function addPost(req,res) {
+    const{...datas}=req.body
+    await postSchema.create({id:req.user.UserID,...datas}).then(()=>{
+        res.status(201).send({msg:"Successfull"})
+    }).catch((error)=>{
+        res.status(404).send({error:error})
+    })  
+}
+
+
+
+export async function showPost(req,res) {
+    const id=req.params.id
+    const post=await postSchema.findOne({_id:id})
+    res.status(200).send({post})
+}
+
+export async function update(req,res) {
+    const {...data}=req.body
+    await postSchema.updateOne({_id:req.params.id},{$set:{id:req.user.UserID,...data}}).then(()=>{
+        res.status(201).send({msg:"updated"})
+    }).catch((error)=>{
+        res.status(500).send({error:error})  
+    })  
+}
+
+export async function adduser(req,res) {
+    const {profile,name,email,phone,pass,cpass}=req.body
+    if(!(name&&email&&pass&&cpass))
+        return res.status(500).send({msg:"empty input"})
+    else if(pass!=cpass)
+        return res.status(500).send({msg:"password missmatch"})
+
+    bcrypt.hash(pass,10).then((hpwd)=>{
+        // console.log(hpwd)
+        console.log("data added");
+        userSchema.create({profile,name,email,phone,pass:hpwd}).then(()=>{
+            res.status(201).send({msg:"Successfull"})
+        }).catch((error)=>{
+            res.status(404).send({error:error})
+        })  
+    }).catch((error)=>{
+        console.log(error)
+    }) 
+}
+
+export async function deleteUser(req, res) { 
+    const { id } = req.params;  
+    const data = await userSchema.deleteOne({ _id: id })
+        .then(() => {
+            res.status(201).send({ msg: "Deleted" });
+        })
+        .catch((error) => {
+            res.status(500).send({ error });
+        });
+}
+
+export async function deletePost(req, res) {
+    // console.log(req.params); 
+    const { id } = req.params;  
+    const data = await postSchema.deleteOne({ _id: id })
+        .then(() => {
+            res.status(201).send({ msg: "Deleted" });
+        })
+        .catch((error) => {
+            res.status(500).send({ error });
+        });
+}
---
+++ router.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+
+import * as rh from './requesthandler.js'
+import Auth from "./middle/Auth.js";
+
+const router=Router();
+
+router.route('/adduser').post(rh.adduser)
+router.route('/login').post(rh.login)
+router.route('/getUser').get(Auth,rh.getUser)
+router.route('/getUserDetails/').get(Auth,rh.getUserDetails)
+router.route('/addpost').post(Auth,rh.addPost)
+router.route('/showPost/:id').get(rh.showPost)
+router.route('/update/:id').put(Auth,rh.update)
+router.route('/deleteUser/:id').delete(rh.deleteUser)
+router.route('/deletePost/:id').delete(rh.deletePost)
+
+
+export default router;
